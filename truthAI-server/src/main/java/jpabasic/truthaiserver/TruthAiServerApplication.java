e2cee6ba124dd04cd18803bfc2e0ed3548f71582
minsikk <131092169+1224kang@users.noreply.github.com>
2025-07-19 15:17:13 +0900

    [chore] spring 기본 세팅

---
+++ truthAI-server/src/main/java/jpabasic/truthaiserver/TruthAiServerApplication.java
@@ -0,0 +1,13 @@
+package jpabasic.truthaiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruthAiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruthAiServerApplication.class, args);
+    }
+
+}
